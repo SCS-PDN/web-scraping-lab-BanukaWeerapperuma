eae670c4aaab2e363ee133f8ba77a4802c8c3165
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2025-04-25 16:05:21 +0530

    Update WebScraper.java

--- WebScraper.java
+++ WebScraper.java
@@ -1,9 +1,39 @@
+import java.io.IOException;
+import java.util.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import java.io.IOException;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WebScraper {
-    public static void main(String[] args) {
-        // TODO: Scrape a URL and print its title
+    public static Map<String, List<String>> scrape(String url, List<String> options) throws IOException {
+        Map<String, List<String>> data = new HashMap<>();
+        Document doc = Jsoup.connect(url).get();
+
+        if (options.contains("title")) {
+            data.put("title", Collections.singletonList(doc.title()));
+        }
+
+        if (options.contains("headings")) {
+            List<String> headings = new ArrayList<>();
+            for (int i = 1; i <= 6; i++) {
+                Elements elems = doc.select("h" + i);
+                for (Element e : elems) {
+                    headings.add("h" + i + ": " + e.text());
+                }
+            }
+            data.put("headings", headings);
+        }
+
+        if (options.contains("links")) {
+            List<String> links = new ArrayList<>();
+            Elements elems = doc.select("a[href]");
+            for (Element e : elems) {
+                links.add(e.attr("abs:href") + " - " + e.text());
+            }
+            data.put("links", links);
+        }
+
+        return data;
     }
 }
